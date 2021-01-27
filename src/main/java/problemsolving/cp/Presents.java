abf2dcbb666b1f20089c46b82ef62edeeb3b4127
pillaiharish <harishpillai1994@gmail.com>
2021-01-27 23:42:48 +0530

    Adding Presents from cfladders

--- src/main/java/problemsolving/cp/Presents.java
+++ src/main/java/problemsolving/cp/Presents.java
@@ -1,5 +1,29 @@
 package problemsolving.cp;
 
-public class Presents {
+import java.util.Scanner;
 
+public class Presents {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		 /*
+		  * here the index starts from 1 and each value is an index 
+		  * for other array. New array to store values from older 
+		  * array.
+		  */
+		
+		int arr2[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr2[arr[i]-1]=i+1;
+		}
+		for(int i=0;i<n;i++) {
+			System.out.print(arr2[i]+" ");
+		}
+		sc.close();
+
+	}
 }
